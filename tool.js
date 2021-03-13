05c12517264006c94c6202b1cc7dfe9fee83527a
fadilkun45 <indocrafter76@gmail.com>
2021-03-14 02:48:19 +0700

    improve design

--- tool.js
+++ tool.js
@@ -1,35 +1,38 @@
 const inputVal = document.querySelector(".search input");
 const render = document.querySelector("#render");
 const btn = document.querySelector(".search button");
+const renderLoad = document.querySelector(".preload")
+ const navBrand = document.querySelector(".navbrand")
 
 btn.addEventListener('click',function(){
     render.innerHTML = '';
+    renderLoad.classList.add("loadingScreen");
+    setTimeout(search,3000)
 })
 
 function search () {
     fetch('https://api.jikan.moe/v3/search/anime?q=' + inputVal.value )
     .then(res => res.json())
     .then(res => {
-        for(i = 0 ; i <= 3 ; i++ ){
-        const result = res.results ;
-
-        let hasil = result[i] ;
-        let gambar = hasil.image_url
-        let title = hasil.title
-        let score = hasil.score
-        let sipnosis = hasil.synopsis
-        let  episode = hasil.episodes
-        let url = hasil.url
-
-        render.innerHTML += `<div class="card">
-        <img src="${gambar}">
-        <h5>${title}</h5>
-        <h5> score : ${score}</h5>
-        <h5> episode : ${episode}</h5>
-        <p>${sipnosis}</p>
-        <a href="${url}">more</a>
-        </div>` ;
-    }
+            for(i = 0 ; i <= 3 ; i++ ){
+                const result = res.results ;
+                let hasil = result[i] ;
+                let gambar = hasil.image_url
+                let title = hasil.title
+                let score = hasil.score
+                let sipnosis = hasil.synopsis
+                let  episode = hasil.episodes
+                let url = hasil.url
+                renderLoad.classList.remove("loadingScreen");
+                render.innerHTML += `<div class="card">
+                <img src="${gambar}">
+                <h5>${title}</h5>
+                <h5> score : ${score}</h5>
+                <h5> episode : ${episode}</h5>
+                <p>${sipnosis}</p>
+                <a href="${url}">more</a>
+                </div>` ;
+            }
     
  })
 }
